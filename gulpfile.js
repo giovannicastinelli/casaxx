151e225621f2768ebc5a844320c7bca3578ffd64
giovannicastinelli <giovannicastinelli@gmail.com>
2017-04-22 10:05:39 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Setup and sync
+var gulp = require("gulp"),
+	bsync = require("browser-sync").create(),
+	sass = require("gulp-sass"),
+	aprefix = require("autoprefixer"),
+	postcss = require("gulp-postcss");
+// gulp.task("task_name, [depedencies]", function(){});
+// Inicio Server
+gulp.task("serve", function () {
+	return bsync.init({
+		server: {
+			baseDir: "./"
+		},
+		online: false
+	});
+});
+// Auto refresh
+gulp.task("run", ["serve"], function () {
+	gulp.watch("*.html").on("change", bsync.reload)
+	gulp.watch("JS/*.js").on("change", bsync.reload)
+	gulp.watch("SASS/*.scss", ["sass"])
+	gulp.watch("CSS/*.css").on("change", bsync.reload)
+})
+gulp.task("sass", function () {
+	return gulp.src("SASS/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(postcss([aprefix("Last 2 versions")]))
+		.pipe(gulp.dest("CSS/"))
+		.pipe(bsync.reload({
+			stream: true
+		}))
+});
